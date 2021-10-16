c4a3a29aab9f90faa9cf44032c6f7e61a57c3278
Your Name <nelliebosch@Nellies-MacPro.lan>
2021-10-15 23:37:20 -0400

    attempt with json

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import stocks from '../data.json'
+import { Link } from 'react-router-dom'
+
+const Dashboard = () => {
+    return(
+        <div>
+            <h1>This is my Dashboard page</h1>
+            {stocks.map((stock) => {
+                const {name, symbol, lastPrice} = stock;
+
+                return(
+                    <Link to={`/stocks/${symbol}`}>
+                        <h2>{name}</h2>
+                    </Link>
+                )
+            })}
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/pages/Stock.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+//import stocks from "../data.json";
+
+const Stock = (props) => {
+
+    //Grabbing the stock symbol from data
+    const symbol = props.match.params.symbol;
+
+    //const url = stocks.symbol
+    
+    //state to hold the stock data
+    const [stock, setStock] = useState(null);
+
+    //function to fetch stock data
+    const getStock = async () => {
+        const response = await fetch(symbol);
+        const data = await response.json();
+        setStock(data);
+    };
+
+    //useEffect to run getStock when component mounts
+    useEffect(() => {
+        getStock();
+    }, []);
+
+    //loaded function for when data is fetched
+    const loaded = () => {
+        return (
+            <div>
+                <h2>{stock.symbol}</h2>
+                <h2>
+                    Name: {stock.name}
+                </h2>
+                <h2>
+                    Price: {stock.lastPrice}
+                </h2>
+            </div>
+        );
+    };
+
+    //Function for when data doesn't exist
+    const loading = () => {
+        return <h1>Loading...</h1>
+    };
+
+
+    //if stock has data, run the loaded function, otherwise, run loading
+    return stock ? loaded() : loading ();
+};
+
+export default Stock;
